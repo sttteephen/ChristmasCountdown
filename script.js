3606ce49b852ab74349cf460d268c00246ad67ef
Stephen Messer <stephenmesser@Stephens-MacBook-Pro.local>
2020-12-24 18:36:41 +0000

    added music

--- script.js
+++ script.js
@@ -1,44 +1,47 @@
 function countdown() {
 	var now = new Date();
-	var christmasDate = new Date(now.getFullYear(), 11, 25);
+	var date = now.getDate()
+	var month = now.getMonth()
+	var minute = now.getMinutes()
 
-	var currentTime = now.getTime();
+	if (minute == 40)//(date == 25 && month == 11)
+	{
+		var x = document.getElementById("myMusic"); 
+  		x.play(); 
+		document.getElementById("chrimbo_suprise").style.visibility= "visible" ;
+		return;
+	}
+	else
+	{
+		var christmasDate = new Date(now.getFullYear(), 11, 25);
 
-	var christmasTime = christmasDate.getTime();
+		var currentTime = now.getTime();
 
-	var remTime = christmasTime - currentTime;
+		var christmasTime = christmasDate.getTime();
 
-	var s = Math.floor(remTime / 1000);
-	var m = Math.floor(s / 60);
-	var h = Math.floor(m / 60);
-	var d = Math.floor(h / 24);
+		var remTime = christmasTime - currentTime;
 
-	s %= 60;
-	m %= 60;
-	h %= 24;
+		var s = Math.floor(remTime / 1000);
+		var m = Math.floor(s / 60);
+		var h = Math.floor(m / 60);
+		var d = Math.floor(h / 24);
 
-	h = (h < 10) ? "0" + h : h;
-	m = (m < 10) ? "0" + m : m;
-	s = (s < 10) ? "0" + s : s;
+		s %= 60;
+		m %= 60;
+		h %= 24;
 
-	document.getElementById("days").innerHTML = d;
-	document.getElementById("hours").innerHTML = h;
-	document.getElementById("minutes").innerHTML = m;
-	document.getElementById("seconds").innerHTML = s;
+		h = (h < 10) ? "0" + h : h;
+		m = (m < 10) ? "0" + m : m;
+		s = (s < 10) ? "0" + s : s;
 
-	setTimeout(countdown, 1000);
+		document.getElementById("days").innerHTML = d;
+		document.getElementById("hours").innerHTML = h;
+		document.getElementById("minutes").innerHTML = m;
+		document.getElementById("seconds").innerHTML = s;
+
+		setTimeout(countdown, 1000);
+	}
 }
 
-var now = new Date();
-//var christmasDate = new Date(now.getFullYear(), 11, 25);
-var date = now.getDate()
-var month = now.getMonth()
 
-if (date == 25 && month == 11)
-{
-	document.getElementById("chrimbo_suprise").style.visibility= "visible" ;
-}
-else
-{
-	countdown();
-}
+countdown();
